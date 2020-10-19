fcca01a4fdfebe29838ab57b1346f8a12602f30e
NelKai <55228995+NelKai@users.noreply.github.com>
2020-10-19 20:07:56 +0300

    Projekti päivitys

--- projekti1.js
+++ projekti1.js
@@ -1,3 +1,39 @@
-function summa() {
+function addItem() {
+    var inputTxt = document.forms.toDoForm.item.value;
+
+    // Tapahtumateksti
+
+    var eventText = document.createElement('p');
+    eventText.style.color = "green";
+    eventText.innerHTML = "Tehtävä lisätty onnistuneesti.";
     
+    var eventText_loc = document.getElementsByTagName('p');
+    eventText_loc[0].appendChild(eventText);
+    
+    // Syötön tarkistus
+
+    if (inputTxt.length < 1) {
+        eventText.style.color = "red";
+        eventText.innerHTML = "Ethän jätä kenttää tyhjäksi.";
+        document.forms.toDoForm.item.style.border = "1px solid red";
+        return false;
+    }
+    
+    // Syötön lisäys listaan
+    
+    var listItem = document.createElement('li');
+
+    listItem.innerHTML = "<li><input type='checkbox' onclick='checkItem()' id='cbox'>" + inputTxt + "</li>";
+    document.getElementById("toDoList").appendChild(listItem);
+     
 }
+
+function checkItem() {
+    alert("Onneksi olkoon, suoritit tehtävän!");
+}
+
+
+
+
+
+
